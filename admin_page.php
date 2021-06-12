06c7c3f535d71283c5fd320adbf6b48950db1b65
BuildTools <unconfigured@null.spigotmc.org>
2021-06-12 19:25:40 +0200

    Started on admin page

---
+++ admin_page.php
@@ -0,0 +1,5 @@
+<?php 
+require('includes/public/admin_dashboard/header.php');
+require('includes/public/admin_dashboard/sidebar.php');
+require('includes/public/admin_dashboard/footer.php');
+?>
--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php 
     require("includes/public/login_site/header.php");
-
 ?>
+<meta name="google-signin-client_id" content="878339401055-rh4p2tjf1recgv9jv72ikvvgnttp6bl6.apps.googleusercontent.com">
+
 <body class="bg-gradient-primary">
     <div class="container">
         <div class="row justify-content-center">
@@ -9,7 +10,6 @@
                 <div class="card shadow-lg o-hidden border-0 my-5">
                     <div class="card-body p-0">
                         <div class="row">
-                            
                             <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
@@ -23,7 +23,11 @@
                                                 <div class="form-check"><input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1"><label class="form-check-label custom-control-label" for="formCheck-1">Remember Me</label></div>
                                             </div>
                                         </div><button class="btn btn-primary d-block btn-user w-100" type="submit">Login</button>
-                                        <hr><a class="btn btn-primary d-block btn-google btn-user w-100 mb-2" role="button"><i class="fab fa-google"></i>&nbsp; Login with Google</a><a class="btn btn-primary d-block btn-facebook btn-user w-100" role="button"><i class="fab fa-facebook-f"></i>&nbsp; Login with Facebook</a>
+                                        <hr>
+                                        <div class="" >
+                                            <a class="g-signin2 btn btn-primary d-block btn-google btn-user w-100 mb-2" role="button" data-onsuccess="onSignIn"><i class="fab fa-google"></i>&nbsp; Login with Google</a>
+                                        </div>
+                                            <a class="btn btn-primary d-block btn-facebook btn-user w-100" role="button"><i class="fab fa-facebook-f"></i>&nbsp; Login with Facebook</a>
                                         <hr>
                                     </form>
                                     <div class="text-center"><a class="small" href="forgot-password.html">Forgot Password?</a></div>
@@ -36,8 +40,18 @@
             </div>
         </div>
     </div>
+    <script>
+        function onSignIn(googleUser) {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        }
+    </script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/chart.min.js"></script>
     <script src="assets/js/bs-init.js"></script>
     <script src="assets/js/theme.js"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
 </body>
