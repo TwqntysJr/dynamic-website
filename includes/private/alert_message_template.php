1d543a7702744b1236cc47bc5df7b6c205c68eec
BuildTools <unconfigured@null.spigotmc.org>
2021-06-13 16:11:05 +0200

    Updates from last night

---
+++ includes/private/alert_message_template.php
@@ -0,0 +1,8 @@
+<a class="dropdown-item d-flex align-items-center" href="#">
+    <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/css/admin_site/img/avatars/avatar2.jpeg">
+        <div class="status-indicator"></div>
+    </div>
+    <div class="fw-bold">
+        <div class="text-truncate"><span>I have the photos that you ordered last month!</span></div>
+        <p class="small text-gray-500 mb-0">Jae Chun - 1d</p>
+    </div>
